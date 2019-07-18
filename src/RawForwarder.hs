3e40cbbbaeb3a58d6d8787e7ab4e94d92c9c75fd
PLR <51248199+plredmond@users.noreply.github.com>
2019-07-18 17:01:31 -0400

    extract conversions to own functions; rename variables for clarity

--- src/RawForwarder.hs
+++ src/RawForwarder.hs
@@ -10,9 +10,9 @@ import qualified Network.Wai as NW
 import qualified Network.Wai.Handler.Warp as NWHW
 
 rawForwarder :: String -> IO ()
-rawForwarder upstreamRaw = do
+rawForwarder rawUpstreamAddress = do
     -- Make a request and a connection manager.
-    request <- NHC.parseRequest upstreamRaw
+    request <- NHC.parseRequest rawUpstreamAddress
     manager <- NHC.newManager NHC.defaultManagerSettings
     -- Run a server to handle downstream requests and fulfill them with
     -- upstream requests.
@@ -21,24 +21,33 @@ rawForwarder upstreamRaw = do
         $ forwardAnyRequest request manager
 
 forwardAnyRequest :: NHC.Request -> NHC.Manager -> NW.Application
-forwardAnyRequest genericRequestForUpstream manager requestFromDownstream respondToDownstream= do
-    -- Make request-for-upstream have the properties of request-from-downstream.
-    bodyHeadingUp <- NW.lazyRequestBody requestFromDownstream
-    let requestForUpstream = genericRequestForUpstream
-            { NHC.method         = NW.requestMethod requestFromDownstream
-            , NHC.path           = NW.rawPathInfo requestFromDownstream -- XXX: Using rawPathInfo might cause bugs? See documentation.
-            , NHC.queryString    = NW.rawQueryString requestFromDownstream
-            , NHC.requestHeaders = NW.requestHeaders requestFromDownstream
-            , NHC.requestBody    = NHC.RequestBodyLBS bodyHeadingUp
-            }
-    -- Execute request-for-upstream.
-    NHC.withResponse requestForUpstream manager $ \responseFromUpstream -> do
-        -- Extract response-from-upstream body.
-        bodyHeadingDown <- fmap DBB.byteString . fmap DB.concat . NHC.brConsume
-            $ NHC.responseBody responseFromUpstream
-        -- Build and return response-to-downstream.
-        respondToDownstream $
-            NW.responseBuilder
-                (NHC.responseStatus responseFromUpstream)
-                (NHC.responseHeaders responseFromUpstream)
-                bodyHeadingDown
+forwardAnyRequest requestTemplate manager downstreamRequest sendResponse = do
+    -- Make a request to send upstream.
+    request <- convertRequest requestTemplate downstreamRequest
+    -- Execute the request and handle the response.
+    NHC.withResponse request manager $ \response -> do
+        -- Make a response to send back downstream.
+        convertResponse response >>= sendResponse
+
+-- | To proxy the Wai request upstream, convert it to a HTTP-Client request.
+convertRequest :: NHC.Request -> NW.Request -> IO NHC.Request
+convertRequest requestTemplate downstreamRequest = do
+    body <- NW.lazyRequestBody downstreamRequest
+    return $ requestTemplate
+        { NHC.method         = NW.requestMethod downstreamRequest
+        , NHC.path           = NW.rawPathInfo downstreamRequest -- XXX: Using rawPathInfo might cause bugs? See documentation.
+        , NHC.queryString    = NW.rawQueryString downstreamRequest
+        , NHC.requestHeaders = NW.requestHeaders downstreamRequest
+        , NHC.requestBody    = NHC.RequestBodyLBS body
+        }
+
+-- | To proxy the HTTP-Client response downstream, convert it to a Wai
+-- response.
+convertResponse :: NHC.Response NHC.BodyReader -> IO NW.Response
+convertResponse upstreamResponse = do
+    body <- fmap DBB.byteString . fmap DB.concat . NHC.brConsume
+        $ NHC.responseBody upstreamResponse
+    return $ NW.responseBuilder
+        (NHC.responseStatus upstreamResponse)
+        (NHC.responseHeaders upstreamResponse)
+        body
