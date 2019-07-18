31660fba1a519169dd72588100facd5945aaf390
PLR <51248199+plredmond@users.noreply.github.com>
2019-07-18 17:06:36 -0400

    rename imports to better distinguish server data/functions from client data/functions

--- src/RawForwarder.hs
+++ src/RawForwarder.hs
@@ -5,49 +5,49 @@ import Network.Wai.Middleware.RequestLogger (logStdout)
 
 import qualified Data.ByteString as DB
 import qualified Data.ByteString.Builder as DBB
-import qualified Network.HTTP.Client as NHC
-import qualified Network.Wai as NW
+import qualified Network.HTTP.Client as Client
+import qualified Network.Wai as Server
 import qualified Network.Wai.Handler.Warp as NWHW
 
 rawForwarder :: String -> IO ()
 rawForwarder rawUpstreamAddress = do
     -- Make a request and a connection manager.
-    request <- NHC.parseRequest rawUpstreamAddress
-    manager <- NHC.newManager NHC.defaultManagerSettings
+    request <- Client.parseRequest rawUpstreamAddress
+    manager <- Client.newManager Client.defaultManagerSettings
     -- Run a server to handle downstream requests and fulfill them with
     -- upstream requests.
     NWHW.runEnv 80
         . logStdout
         $ forwardAnyRequest request manager
 
-forwardAnyRequest :: NHC.Request -> NHC.Manager -> NW.Application
+forwardAnyRequest :: Client.Request -> Client.Manager -> Server.Application
 forwardAnyRequest requestTemplate manager downstreamRequest sendResponse = do
     -- Make a request to send upstream.
     request <- convertRequest requestTemplate downstreamRequest
     -- Execute the request and handle the response.
-    NHC.withResponse request manager $ \response -> do
+    Client.withResponse request manager $ \response -> do
         -- Make a response to send back downstream.
         convertResponse response >>= sendResponse
 
 -- | To proxy the Wai request upstream, convert it to a HTTP-Client request.
-convertRequest :: NHC.Request -> NW.Request -> IO NHC.Request
+convertRequest :: Client.Request -> Server.Request -> IO Client.Request
 convertRequest requestTemplate downstreamRequest = do
-    body <- NW.lazyRequestBody downstreamRequest
+    body <- Server.lazyRequestBody downstreamRequest
     return $ requestTemplate
-        { NHC.method         = NW.requestMethod downstreamRequest
-        , NHC.path           = NW.rawPathInfo downstreamRequest -- XXX: Using rawPathInfo might cause bugs? See documentation.
-        , NHC.queryString    = NW.rawQueryString downstreamRequest
-        , NHC.requestHeaders = NW.requestHeaders downstreamRequest
-        , NHC.requestBody    = NHC.RequestBodyLBS body
+        { Client.method         = Server.requestMethod downstreamRequest
+        , Client.path           = Server.rawPathInfo downstreamRequest -- XXX: Using rawPathInfo might cause bugs? See documentation.
+        , Client.queryString    = Server.rawQueryString downstreamRequest
+        , Client.requestHeaders = Server.requestHeaders downstreamRequest
+        , Client.requestBody    = Client.RequestBodyLBS body
         }
 
 -- | To proxy the HTTP-Client response downstream, convert it to a Wai
 -- response.
-convertResponse :: NHC.Response NHC.BodyReader -> IO NW.Response
+convertResponse :: Client.Response Client.BodyReader -> IO Server.Response
 convertResponse upstreamResponse = do
-    body <- fmap DBB.byteString . fmap DB.concat . NHC.brConsume
-        $ NHC.responseBody upstreamResponse
-    return $ NW.responseBuilder
-        (NHC.responseStatus upstreamResponse)
-        (NHC.responseHeaders upstreamResponse)
+    body <- fmap DBB.byteString . fmap DB.concat . Client.brConsume
+        $ Client.responseBody upstreamResponse
+    return $ Server.responseBuilder
+        (Client.responseStatus upstreamResponse)
+        (Client.responseHeaders upstreamResponse)
         body
